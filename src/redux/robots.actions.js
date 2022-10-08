66e84eed7fab6a437bb0b0bc59fac2185232d6e2
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-08 21:24:36 +0800

    create robot's reducers, state, actions during request of url

--- src/redux/robots.actions.js
+++ src/redux/robots.actions.js
@@ -1,6 +1,38 @@
-import { CHANGE_SEARCH_FIELD } from './robots.types';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS
+} from './robots.types';
+
+// const localUrl = 'http://localhost:3000/robots' - if using server
+const dummyUrl = 'https://jsonplaceholder.typicode.com/user';
 
 export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 });
+
+// using redux-thunk, inception
+export const requestRobots = () => dispatch => {
+  // loading
+  dispatch({
+    type: REQUEST_ROBOTS_PENDING
+  });
+
+  fetch(dummyUrl)
+    .then(resp => resp.json())
+    .then(data =>
+      dispatch({
+        type: REQUEST_ROBOTS_SUCCESS,
+        payload: data
+      })
+    )
+    .catch(error => {
+      console.log(error);
+      dispatch({
+        type: REQUEST_ROBOTS_FAILED,
+        payload: error
+      });
+    });
+};
